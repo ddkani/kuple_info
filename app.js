dc51b1726cb7667c8a3f2a5c8c459b10c502fdec
live2skull <live2skull@gmail.com>
2018-03-03 01:23:25 +0900

    redirect

--- app.js
+++ app.js
@@ -71,9 +71,10 @@ app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    res.redirect('/mobile/haksik')
+    // const err = new Error('Not Found');
+    // err.status = 404;
+    // next(err);
 });
 
 // error handler
