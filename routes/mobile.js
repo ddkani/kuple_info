9ca456275494d489ce88a35158d99fdd77b01224
live2skull <live2skull@gmail.com>
2018-03-14 23:52:34 +0900

    test

--- routes/mobile.js
+++ routes/mobile.js
@@ -3,12 +3,12 @@ let router = express.Router();
 
 /* GET users listing. */
 router.get('/haksik', function(req, res, next) {
-    res.render('mobile/haksik')
+    res.render('mobile/haksik', {title : '쿠플존 - 오늘의 학식'})
 });
 
 
 router.get('/shuttle', function(req, res, next) {
-    res.render('mobile/shuttle')
+    res.render('mobile/shuttle', {title : '쿠플존 - 셔틀버스 시간표'})
 });
 
 
